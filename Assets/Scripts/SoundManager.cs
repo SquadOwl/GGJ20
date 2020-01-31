81cc4b4811c93f54aea4046a588c343fdf1557fc
Eugene Sazonov <eugenius.coder@gmail.com>
2020-01-31 21:22:06 +0200

    Add background sound and SoundManager singleton

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance = null;
+    private AudioSource source;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        if (instance == this) return;
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.Play();
+    }
+}
