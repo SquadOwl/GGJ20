4af038652ec2c8caae7eb023460c28923fa09964
ARTllline <ARTllline@users.noreply.github.com>
2020-01-31 22:11:43 +0200

    playerClass

---
+++ Assets/ARTlineCode/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Vector3 moveVector;
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * moveSpeed;
+        moveVector.z = Input.GetAxis("Vertical") * moveSpeed;
+
+        characterController.Move(moveVector * Time.deltaTime);
+    }
+
+    private void Repair ()
+    {
+
+    }
+
+    private void Attack()
+    {
+
+    }
+
+}
