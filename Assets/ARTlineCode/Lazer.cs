f5b24fad2a069b21fb63a4d4286eb9d4d45fdb9c
ARTllline <ARTllline@users.noreply.github.com>
2020-02-01 10:30:20 +0200

    LazerTest

---
+++ Assets/ARTlineCode/Lazer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Lazer : MonoBehaviour
+{
+    [SerializeField] float lazerOffTime = 10f;
+    [SerializeField] float maxDistance = 150f;
+    LineRenderer lr;
+    bool canFire = false;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    private void Start()
+    {
+        lr.enabled = false;
+    }
+
+    private void Update()
+    {
+        FireLazer(transform.forward * maxDistance);
+    }
+
+    public void FireLazer(Vector3 targetPosition)
+    {
+        if(canFire)
+        {
+            //lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, targetPosition);
+            lr.enabled = true;
+            canFire = false;
+        }
+     
+
+        Invoke("TurnOffLazer", lazerOffTime);
+    }
+
+    void TurnOffLazer()
+    {
+        lr.enabled = false;
+        canFire = true;
+    }
+}
