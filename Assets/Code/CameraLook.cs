b36122d403405bd6b8b6e8703225c7bcc0b853ac
Mamoru <sasha.buzhenko.98@gmail.com>
2020-02-01 15:37:57 +0200

    rotate

---
+++ Assets/Code/CameraLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    GameObject camera;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+
+    //    camera = GameObject.FindGameObjectWithTag("MainCamera");
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    //  transform.LookAt(new Vector3(0, camera.transform.position.x, 0));
+    //    //  camera.transform.LookAt(new Vector3(0,camera.transform.position.x,0));
+
+    //    transform.LookAt(new Vector3(camera.transform.position.y,0,0));
+    //}
+
+
+    public double dx;
+    public double dy;
+
+    // Use this for initialization
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(new Vector3(0, 0, 5));
+    }
+}
