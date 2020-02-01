57b6c00b3ed0b5bac94c7f0fc8c00138604d9d89
metalkorshik <nik1998121212@gmail.com>
2020-02-01 14:36:25 +0200

    menu commit

---
+++ Assets/MenuCode/SwitchGameMode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchGameMode : MonoBehaviour
+{
+    public GameObject UsersPanel;
+    public GameObject oppositeButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UsersPanel.SetActive(false);
+        bool IsSinglePlayer = gameObject.tag == "singlePlayerBtn";
+        var button = GetComponent<Button>();
+
+        button.onClick.AddListener(delegate
+        {
+            button.GetComponentsInChildren<Image>()[1].color = Color.cyan;
+            oppositeButton.GetComponentsInChildren<Image>()[1].color = Color.magenta;
+
+            if (IsSinglePlayer)
+                UsersPanel.SetActive(false);
+            else
+                UsersPanel.SetActive(true);
+        });
+    }
+
+}
---
+++ Assets/MenuCode/SwitchViews.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SwitchViews : MonoBehaviour
+{
+    public GameObject fadingView;
+    public GameObject popUpView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        (GetComponent<Button>()).onClick.AddListener(delegate {
+
+            if(fadingView != null)
+                fadingView.SetActive(false);
+
+            if (popUpView != null)
+                popUpView.SetActive(true);
+
+        });
+    }
+
+   
+}
