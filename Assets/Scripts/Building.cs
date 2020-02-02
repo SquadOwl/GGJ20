27355bd101955073d0eeb50e5a272c54c9417eca
Eugene Sazonov <eugenius.coder@gmail.com>
2020-02-02 11:00:38 +0200

    Add board script and url button

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Building : MonoBehaviour
+{
+
+    private int health = 0;
+
+    public int repairedHealth;
+
+    public bool isRepaired;
+
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void GetRepair(int count)
+    {
+        health += count;
+        if(health >= repairedHealth)
+        {
+            isRepaired = true;
+        }
+    }
+}
