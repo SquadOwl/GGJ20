eb1945e327a6326de07a601a8c7b8e6e206f2abb
Mamoru <sasha.buzhenko.98@gmail.com>
2020-02-04 22:20:29 +0200

    Start update

---
+++ Assets/Code/NikLazCode/SwitchGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchGame : MonoBehaviour
+{
+    public GameObject ARCamera;
+    public GameObject MenuAll;
+    public GameObject ImageTarget;
+    public GameObject MainMusic;
+    public GameObject MenuPanel;
+    public bool IsPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(delegate {
+
+            ARCamera.SetActive(!IsPause);
+            MenuAll.SetActive(!IsPause);
+            ImageTarget.SetActive(!IsPause);
+            MainMusic.SetActive(!IsPause);
+            MenuPanel.SetActive(IsPause);
+
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
