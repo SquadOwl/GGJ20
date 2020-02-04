37f4484068dbd90ff54788807b2071615f8ad4ac
Eugene Sazonov <eugenius.coder@gmail.com>
2020-02-04 20:46:44 +0200

    Fixed sound manager

--- Assets/Code/SoundManager.cs
+++ Assets/Code/SoundManager.cs
@@ -1,18 +1,82 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : Singleton<SoundManager>
 {
-    public AudioSource source;
 
-    public void PlayLose()
+    public Sound[] sounds;
+    
+    protected override void Awake()
     {
-        source.Play();
+        base.Awake();
+    
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            if (s.isMusic)
+            {
+                s.source.volume = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+            }
+            else
+            {
+                s.source.volume = PlayerPrefs.GetFloat("SoundsVolume", 1);
+            }
+        }
+
+
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Play("Intro");
+        }
+        else
+        {
+            Play("Soundtrack");
+        }
     }
 
-    public void ChangeSound(float value)
+
+    public void Play(string name)
     {
-        source.volume = value;
+        foreach (Sound s in sounds)
+        {
+            if (s.name == name) s.source.Play();
+        }
+
+    }
+
+    public void Stop(string name)
+    {
+        foreach(Sound s in sounds)
+        {
+            if (s.name == name) s.source.Stop();
+        }
+    }
+
+    public void ChangeMusicVolume(float value)
+    {
+        Debug.Log("vol is: " + value);
+        foreach (Sound s in sounds)
+        {
+            if(s.isMusic)
+                s.source.volume = value;
+        }
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+    
+
+    public void ChangeSoundsVolume(float value)
+    {
+        foreach (Sound s in sounds)
+        {
+            if(!s.isMusic)
+                s.source.volume = value;
+        }
+        PlayerPrefs.SetFloat("SoundsVolume", value);
     }
 }
